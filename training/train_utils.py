ceb428fa78766ea358d1ab86ae672bf45ee30a0f
apurvthapa <apurvthapa@gmail.com>
2026-04-17 21:41:26 +0530

    added ML integration

---
+++ training/train_utils.py
@@ -0,0 +1,7 @@
+import os
+
+APP_DIR = 'app'
+MODEL_DIR_NAME = 'models'
+MODEL_NAME = 'model.joblib'
+MODEL_DIR = os.path.join(APP_DIR, MODEL_DIR_NAME)
+MODEL_PATH = os.path.join(MODEL_DIR, MODEL_NAME)
